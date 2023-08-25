f7e2267f7d8a1e104d17a16a03a6518cb74547b8
musty1406 <musty1406@gmail.com>
2023-08-25 12:23:46 +0300

    25/08/2023  gun sonu

---
+++ config-server/src/main/java/com/bilgeadam/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.bilgeadam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class);
+    }
+}
