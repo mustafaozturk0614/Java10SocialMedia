05189c3dfa633d0a9f5a9ed9eccd4da191ef97e2
musty1406 <musty1406@gmail.com>
2023-08-29 12:36:32 +0300

    29/08/2023 gun sonu

---
+++ mail-service/src/main/java/com/bilgeadam/MailServiceApplication.java
@@ -0,0 +1,31 @@
+package com.bilgeadam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@SpringBootApplication
+public class MailServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MailServiceApplication.class);
+    }
+
+    //deneme amaçlı kod
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public  void sendMail(){
+        SimpleMailMessage mailMessage=new SimpleMailMessage();
+        mailMessage.setFrom("${java10_mail}");
+        mailMessage.setTo("java9and10@gmail.com");
+        mailMessage.setSubject("AKTIVASYON KODUNUZ...");
+        mailMessage.setText("lkD345");
+        javaMailSender.send(mailMessage);
+    }
+
+}
